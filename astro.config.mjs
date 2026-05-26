6992347c203beb8389cef701e27fd320f40800c3
wublabu <151746936+wublabu@users.noreply.github.com>
2026-05-26 20:57:18 +0800

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === "development") {
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.site_url,
+	site: 'https://wublabu.github.io',
 	
 	base: "/",
 	trailingSlash: "always",
