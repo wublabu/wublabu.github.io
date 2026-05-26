98132f7336e31b5bd0bcc3cfc912f1a7ad4a09b4
wublabu <151746936+wublabu@users.noreply.github.com>
2026-05-26 21:09:37 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -10,10 +10,10 @@ export const siteConfig: SiteConfig = {
 	title: "Firefly",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "dudududu",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://wublabu.github.io/",
 
 	// 站点描述
 	description:
